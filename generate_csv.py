773eba9f758e992dec646039fc2e114b47a61190
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2020-12-01 17:03:47 +0000

    Add disability variables

--- generate_csv.py
+++ generate_csv.py
@@ -23,7 +23,10 @@ PERSON_FIELDNAMES = [
     "total_benefits",
     "is_household_head",
     "is_benunit_head",
-    "FRS_net_income"
+    "FRS_net_income",
+    "registered_disabled",
+    "dis_equality_act_core",
+    "dis_equality_act_wider"
 ] + [benefit for benefit in BENEFITS.values() if benefit not in BENUNIT_LEVEL_BENEFITS and benefit in REPORTED] + [benefit + "_reported" for benefit in BENEFITS.values() if benefit not in BENUNIT_LEVEL_BENEFITS and benefit in SIMULATED and benefit]
 
 BENUNIT_FIELDNAMES = [
@@ -162,6 +165,9 @@ def parse_adult(line, person):
     person["is_household_head"] = int(line["COMBID"]) == 1
     person["is_benunit_head"] = int((line["COMBID"] == "1") or (line["UPERSON"] == "1"))
     person["FRS_net_income"] = adjust_period(safe(line["NINDINC"]), WEEK, YEAR) - person["misc_income"]
+    person["registered_disabled"] = safe(line["LAREG"]) == 1
+    person["dis_equality_act_core"] = safe(line["DISCORA1"]) == 1
+    person["dis_equality_act_wider"] = safe(line["DISACTA1"]) == 1
     return person
 
 def parse_childcare(line, person):
@@ -178,6 +184,9 @@ def parse_child(line, person):
     person["misc_income"] = adjust_period(line["CHRINC"], WEEK, YEAR)
     person["earnings"] = adjust_period(line["CHEARNS"], WEEK, YEAR)
     person["FRS_net_income"] = safe(line["CHINCDV"]) - person["misc_income"]
+    person["registered_disabled"] = safe(line["LAREG"]) == 1
+    person["dis_equality_act_core"] = safe(line["DISCORC1"]) == 1
+    person["dis_equality_act_wider"] = safe(line["DISACTC1"]) == 1
     return person
 
 def parse_job(line, person):
