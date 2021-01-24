ff09aa1a8597344aa01c3c12a0d5c739cba7f8e6
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-01-24 12:18:39 +0000

    Integrate parse submodules

---
+++ frs/tables/__init__.py
@@ -0,0 +1,4 @@
+from frs.tables.adult import parse_adult, ADULT_FIELDNAMES
+from frs.tables.accounts import parse_account, ACCOUNTS_FIELDNAMES
+
+PERSON_FIELDNAMES = ADULT_FIELDNAMES + ACCOUNTS_FIELDNAMES
